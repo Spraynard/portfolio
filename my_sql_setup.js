751c66893846032384af87ac4b7c1943e6d27a60
Spraynard <kellan.martin@gmail.com>
2017-06-20 15:20:45 +0000

    added sql setup to gitignore

--- my_sql_setup.js
+++ my_sql_setup.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql')
 var pool = mysql.createPool ({
 	host: 'localhost',
-	user: 'kellan_martin',
+	user: 'kwmartin',
 	password: 'Limester91!',
 	database: 'kwm_portfolio'
 })
